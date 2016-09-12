bfa57afdceaec2727575a3593b70406f0039cbc1
Kandadai Madhurima Yasaswini <Madhurima_Kandadai@epam.com>
2016-09-12 09:47:35 +0530

    Added files into the repository.

---
+++ JobPortal.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JobPortal.Web.Startup))]
+namespace JobPortal.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
